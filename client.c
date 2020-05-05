77b6ec840dd6c290c1f04ed8a8a62f2767376677
maria <maria.leoveanu@yahoo.com>
2020-05-05 19:51:53 +0300

    get_books, get_book, delete_book functional

--- client.c
+++ client.c
@@ -35,11 +35,12 @@ char* restore_connection(int* sockfd, char* message, char* response){
 
 int main(int argc, char *argv[])
 {
-    char *message;
-    char *response;
-    int sockfd;
-    char *command;
+    char* message;
+    char* response;
+    char* command;
     char* cookies;
+    char* token;
+    int sockfd;
     command = malloc(sizeof(char) * 101);
     sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
         
@@ -71,12 +72,12 @@ int main(int argc, char *argv[])
     		login_data[0] = json_serialize_to_string_pretty(root_value);
     		cont_type = "application/json";
     		
-    		message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/register", cont_type, login_data, 1, NULL, 0);
+    		message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/register", cont_type, login_data, 1, NULL, 0, NULL, 0);
     		printf("%s\n", message);
     		send_to_server(sockfd, message);
     		response = receive_from_server(sockfd);
     		if(strlen(response) == 0){
-    		 	message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/register", cont_type, login_data, 1, NULL, 0);
+    		 	message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/register", cont_type, login_data, 1, NULL, 0, NULL, 0);
     		 	printf("%s\n", message);
     		 	sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
     		 	send_to_server(sockfd, message);
@@ -110,14 +111,14 @@ int main(int argc, char *argv[])
     		json_object_set_string(root_object, "password", password);
     		user_info[0] = json_serialize_to_string_pretty(root_value);
 
-    		message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/login", cont_type, user_info, 1, NULL, 0);
+    		message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/login", cont_type, user_info, 1, NULL, 0, NULL, 0);
     		printf("%s\n", message);
     		send_to_server(sockfd, message);
     		response = receive_from_server(sockfd);
     		while(strlen(response) == 0){
 
     			// while there is no response from server, restablish connection and try again
-    			message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/login", cont_type, user_info, 1, NULL, 0);
+    			message = compute_post_request("3.8.116.10", "/api/v1/tema/auth/login", cont_type, user_info, 1, NULL, 0, NULL, 0);
      			printf("%s\n", message);
      			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
     			send_to_server(sockfd, message);
@@ -133,78 +134,153 @@ int main(int argc, char *argv[])
     	}
     	if(strcmp(command, "enter_library") == 0){
 
-     		message = compute_get_request("3.8.116.10", "/api/v1/tema/library/access", NULL, &cookies, 1);
+     		message = compute_get_request("3.8.116.10", "/api/v1/tema/library/access", NULL, &cookies, 1, NULL, 0);
      		printf("%s\n", message);
      		send_to_server(sockfd, message);
      		response = receive_from_server(sockfd);
      		while(strlen(response) == 0){
 
      			// while there is no response from server, restablish connection and try again
-     			message = compute_get_request("3.8.116.10", "/api/v1/tema/library/access", NULL, &cookies, 1);
+     			message = compute_get_request("3.8.116.10", "/api/v1/tema/library/access", NULL, &cookies, 1, NULL, 0);
      			printf("%s\n", message);
      			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
     			send_to_server(sockfd, message);
     			response = receive_from_server(sockfd);
      		}
      		printf("%s\n", response);
+
+     		// extract token
+     		char* start_pattern = "{\"token\":\"";
+     		char* end_pattern = "\"}";
+     		token = extract_info_response(start_pattern, end_pattern, response);
+     		printf("Extracted token:%s\n", token);
      		
     	}
+
+    	if(strcmp(command, "get_books") == 0){
+    		
+    		message = compute_get_request("3.8.116.10", "/api/v1/tema/library/books", NULL, NULL, 0, &token, 1);
+    		printf("%s\n", message);
+    		send_to_server(sockfd, message);
+    		response = receive_from_server(sockfd);
+    		while(strlen(response) == 0){
+    			message = compute_get_request("3.8.116.10", "/api/v1/tema/library/books", NULL, NULL, 0, &token, 1);
+    			printf("%s\n", message);
+    			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
+    			send_to_server(sockfd, message);
+    			response = receive_from_server(sockfd);
+    		}
+    		printf("%s\n", response);
+    	}
+
+    	if(strcmp(command, "add_book") == 0){
+
+    		char *title, *author, *genre, *publisher;
+    		char* cont_type = "application/json";
+    		char* book_info[1];
+    		int page_count;
+
+    		title = malloc(sizeof(char) * 101);
+    		author = malloc(sizeof(char) * 101);
+    		genre = malloc(sizeof(char) * 101);
+    		publisher = malloc(sizeof(char) * 101);
+
+    		JSON_Value *root_value = json_value_init_object();
+    		JSON_Object *root_object = json_value_get_object(root_value);
+
+    		printf("title:");
+    		scanf("%s", title);
+    		printf("author:");
+    	    scanf("%s", author);
+    	    printf("genre:");
+    	    scanf("%s", genre);
+    	    printf("page_count:");
+    	    scanf("%d", &page_count);
+    	    printf("publisher:");
+    	    scanf("%s", publisher);
+
+    	    json_object_set_string(root_object, "title", title);
+    		json_object_set_string(root_object, "author", author);
+    		json_object_set_string(root_object, "genre", genre);
+    		json_object_set_number(root_object, "page_count", page_count);
+    		json_object_set_string(root_object, "publisher", publisher);
+
+    		book_info[0] = json_serialize_to_string_pretty(root_value);
+
+    		message = compute_post_request("3.8.116.10", "/api/v1/tema/library/books", cont_type, book_info, 1, NULL, 0, &token, 1);
+    		printf("%s\n", message);
+    		send_to_server(sockfd, message);
+    		response = receive_from_server(sockfd);
+    		while(strlen(response) == 0){
+    			message = compute_post_request("3.8.116.10", "/api/v1/tema/library/books", cont_type, book_info, 1, NULL, 0, &token, 1);
+    			printf("%s\n", message);
+    			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
+    			send_to_server(sockfd, message);
+    			response = receive_from_server(sockfd);
+    		}
+
+    		printf("%s\n", response);
+
+    	}
+
+    	if(strcmp(command, "get_book") == 0){
+    		
+    		char* id = malloc(sizeof(char) * 101);
+    		char* final_path = malloc(sizeof(char) * 101);
+    		char* path = "/api/v1/tema/library/books/";
+
+    		printf("id:");
+    		scanf("%s", id);
+    		
+    		strcpy(final_path, path);
+    		strcat(final_path, id);
+    		printf("%s\n", final_path);
+
+    		message = compute_get_request("3.8.116.10", final_path, NULL, NULL, 0, &token, 1);
+    		printf("%s\n", message);
+    		send_to_server(sockfd, message);
+    		response = receive_from_server(sockfd);
+    		while(strlen(response) == 0){
+				message = compute_get_request("3.8.116.10", final_path, NULL, NULL, 0, &token, 1);    			
+				printf("%s\n", message);
+    			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
+    			send_to_server(sockfd, message);
+    			response = receive_from_server(sockfd);
+    		}
+
+    		printf("%s\n", response);
+
+    	}
+    	if(strcmp(command, "delete_book") == 0){
+    		
+    		char* id = malloc(sizeof(char) * 101);
+    		char* final_path = malloc(sizeof(char) * 101);
+    		char* path = "/api/v1/tema/library/books/";
+
+    		printf("id:");
+    		scanf("%s", id);
+    		
+    		strcpy(final_path, path);
+    		strcat(final_path, id);
+    		printf("%s\n", final_path);
+
+    		message = compute_delete_request("3.8.116.10", final_path, NULL, NULL, 0, NULL, 0, &token, 1);
+    		printf("%s\n", message);
+    		send_to_server(sockfd, message);
+    		response = receive_from_server(sockfd);
+    		while(strlen(response) == 0){
+				message = compute_delete_request("3.8.116.10", final_path, NULL, NULL, 0, NULL, 0, &token, 1);    			
+				printf("%s\n", message);
+    			sockfd = open_connection("3.8.116.10", 8080, AF_INET, SOCK_STREAM, 0);
+    			send_to_server(sockfd, message);
+    			response = receive_from_server(sockfd);
+    		}
+
+    		printf("%s\n", response);
+
+    	}
+
     }
     
-
-    // Ex 1.2: POST dummy and print response from main server
-    // char* data[1];
-    // data[0] = "mesaj=dummy";
-    // char* cont_type = "application/x-www-form-urlencoded";
-    // message = compute_post_request("3.8.116.10", "/api/v1/dummy", cont_type, data, 1, NULL, 0);
-    // puts(message);
-    // send_to_server(sockfd, message);
-    // response = receive_from_server(sockfd);
-    // printf("%s\n", response);
-
-    // Ex 2: Login into main server
-    // char* login_data[2];
-    // login_data[0] = "username=student";
-    // login_data[1] = "password=student";
-    // cont_type = "application/x-www-form-urlencoded";
-    // message = compute_post_request("3.8.116.10", "/api/v1/auth/login", cont_type, login_data, 2, NULL, 0);
-    // puts(message);
-    // send_to_server(sockfd, message);
-    // response = receive_from_server(sockfd);
-    // printf("%s\n", response);
-
-    // Ex 3: GET weather key from main server
-     
-
-    // Ex 4: GET weather data from OpenWeather API
-    // int weather_socket = open_connection("37.139.1.159", 80, AF_INET, SOCK_STREAM, 0);
-    // char* parameter = "lat=18&lon=34&appid=b912dd495585fbf756dc6d8f415a7649";
-    // message = compute_get_request("37.139.1.159", "/data/2.5/weather", parameter, NULL, 0);
-    // puts(message);
-    // send_to_server(weather_socket, message);
-    // response = receive_from_server(weather_socket);
-    // printf("%s\n",response);
-
-    // Ex 5: POST weather data for verification to main server
-    // cont_type = "appliction/json";
-    // char* content[1];
-    // content[0] = basic_extract_json_response(response);
-    // message = compute_post_request("3.8.116.10", "/api/v1/weather/18/34", cont_type, content, 1, cookies, 1);
-    // puts(message);
-    // send_to_server(sockfd, message);
-    // response = receive_from_server(sockfd);
-    // printf("%s\n",response);
-
-    // Ex 6: Logout from main server
-    // message = compute_get_request("3.8.116.10", "/api/v1/auth/logout", NULL, NULL, 0);
-    // puts(message);
-    // send_to_server(sockfd, message);
-    // response = receive_from_server(sockfd);
-    // printf("%s\n",response);
-
-    // BONUS: make the main server return "Already logged in!"
-
-    // free the allocated data at the end!
-
     return 0;
 }
--- requests.c
+++ requests.c
@@ -10,7 +10,7 @@
 #include "requests.h"
 
 char *compute_get_request(char *host, char *url, char *query_params,
-                            char **cookies, int cookies_count)
+                            char **cookies, int cookies_count, char** tokens, int tokens_count)
 {
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
@@ -40,6 +40,17 @@ char *compute_get_request(char *host, char *url, char *query_params,
        compute_message(message, line);
     }
 
+    if(tokens != NULL){
+        memset(line, 0, LINELEN);
+        sprintf(line, "Authorization: Bearer ");
+        for(int i = 0; i < tokens_count; i++){
+            char add[1000];
+            sprintf(add, "%s", tokens[i]);
+            strcat(line, add);
+        }
+        compute_message(message, line);
+    }
+
     // Step 4: add final new line
     compute_message(message, "");
     free(line);
@@ -48,7 +59,7 @@ char *compute_get_request(char *host, char *url, char *query_params,
 }
 
 char *compute_post_request(char *host, char *url, char* content_type, char **body_data,
-                            int body_data_fields_count, char **cookies, int cookies_count)
+                            int body_data_fields_count, char **cookies, int cookies_count, char** tokens, int tokens_count)
 {
     char *message = calloc(BUFLEN, sizeof(char));
     char *line = calloc(LINELEN, sizeof(char));
@@ -63,7 +74,7 @@ char *compute_post_request(char *host, char *url, char* content_type, char **bod
     memset(line, 0, LINELEN);
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
-    
+
     /* Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
             in order to write Content-Length you must first compute the message size
     */
@@ -94,6 +105,91 @@ char *compute_post_request(char *host, char *url, char* content_type, char **bod
        }
        compute_message(message, line);
     }
+
+    if(tokens != NULL){
+        memset(line, 0, LINELEN);
+        sprintf(line, "Authorization: Bearer ");
+        for(int i = 0; i < tokens_count; i++){
+            char add[1000];
+            sprintf(add, "%s", tokens[i]);
+            strcat(line, add);
+        }
+        compute_message(message, line);
+    }
+
+
+    // Step 5: add new line at end of header
+    compute_message(message, "");
+
+    // Step 6: add the actual payload data
+    memset(line, 0, LINELEN);
+    if(body_data != NULL){
+        compute_message(message, body_data_buffer);
+    }
+    
+    free(line);
+    return message;
+}
+
+char *compute_delete_request(char *host, char *url, char* content_type, char **body_data,
+                            int body_data_fields_count, char **cookies, int cookies_count, char** tokens, int tokens_count)
+{
+    char *message = calloc(BUFLEN, sizeof(char));
+    char *line = calloc(LINELEN, sizeof(char));
+    char *body_data_buffer = calloc(LINELEN, sizeof(char));
+
+    // Step 1: write the method name, URL and protocol type
+    memset(line, 0, LINELEN);
+    sprintf(line, "DELETE %s HTTP/1.1", url);
+    compute_message(message, line);
+    
+    // Step 2: add the host
+    memset(line, 0, LINELEN);
+    sprintf(line, "Host: %s", host);
+    compute_message(message, line);
+
+    /* Step 3: add necessary headers (Content-Type and Content-Length are mandatory)
+            in order to write Content-Length you must first compute the message size
+    */
+    memset(line ,0, LINELEN);
+    sprintf(line, "Content-Type: %s", content_type);
+    compute_message(message, line);
+
+    if(body_data != NULL){
+        memset(body_data_buffer, 0, LINELEN);
+        for(int i = 0; i < body_data_fields_count; i++){
+            char extra[100];
+            sprintf(extra, "%s", body_data[i]);
+            strcat(body_data_buffer, extra);
+        }
+    }
+    memset(line, 0, LINELEN);
+    sprintf(line, "Content-Length: %ld", strlen(body_data_buffer));
+    compute_message(message, line);
+
+    // Step 4 (optional): add cookies
+    if (cookies != NULL) {
+       memset(line, 0, LINELEN);
+       sprintf(line, "Cookie: ");
+       for(int i = 0; i < cookies_count; i++){
+            char add[100];
+            sprintf(add, "%s;", cookies[i]);
+            strcat(line, add);
+       }
+       compute_message(message, line);
+    }
+
+    if(tokens != NULL){
+        memset(line, 0, LINELEN);
+        sprintf(line, "Authorization: Bearer ");
+        for(int i = 0; i < tokens_count; i++){
+            char add[1000];
+            sprintf(add, "%s", tokens[i]);
+            strcat(line, add);
+        }
+        compute_message(message, line);
+    }
+
 
     // Step 5: add new line at end of header
     compute_message(message, "");
